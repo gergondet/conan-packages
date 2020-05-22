b30d977ac5075af40293c343c23904d83dd80b6d
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-05-22 16:32:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This repository hosts some recipes originally found on [conan-center].
+
+Those have two notable differences:
+- default to shared libraries
+- do not remove CMake config script
+
+Binary builds are limited to the configuration in GitHub Actions
---
+++ yaml-cpp/all/conanfile.py
@@ -0,0 +1,59 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class YamlCppConan(ConanFile):
+    name = "yaml-cpp"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/jbeder/yaml-cpp"
+    topics = ("conan", "yaml", "yaml-parser", "serialization", "data-serialization")
+    description = "A YAML parser and emitter in C++"
+    license = "MIT"
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": True, "fPIC": True}
+    _source_subfolder = "source_subfolder"
+    version = "0.6.3"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def configure(self):
+        if self.settings.compiler == "Visual Studio" and self.settings.compiler.version == "12":
+            raise ConanInvalidConfiguration("Visual Studio 12 not supported: Library needs C++11 standard")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["YAML_CPP_BUILD_TESTS"] = False
+        cmake.definitions["YAML_CPP_BUILD_CONTRIB"] = True
+        cmake.definitions["YAML_CPP_BUILD_TOOLS"] = False
+        cmake.definitions["YAML_BUILD_SHARED_LIBS"] = self.options.shared
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["MSVC_SHARED_RT"] = "MD" in self.settings.compiler.runtime
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append('m')
+        if self.settings.compiler == 'Visual Studio':
+            self.cpp_info.defines.append('_NOEXCEPT=noexcept')
